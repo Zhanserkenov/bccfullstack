81d90158c5ceb3f929ae11d0356350334bdf5b99
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2025-09-14 22:22:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1151,5 +1151,6 @@ def get_stats():
         logger.error(f"Ошибка при получении статистики: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=False, host="0.0.0.0", port=port)
